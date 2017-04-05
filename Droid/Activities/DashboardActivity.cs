e9f118523ef04330dde5c38e54d660be94d4e5cb
Philip Gai <philipmgai@gmail.com>
2017-04-05 12:46:37 -0500

    Updated Creating Table Rows

--- Droid/Activities/DashboardActivity.cs
+++ Droid/Activities/DashboardActivity.cs
@@ -23,46 +23,56 @@ namespace Commercially.Droid
 			SetContentView(Resource.Layout.Dashboard);
 
 			Home.PrefetchData();
-			Initialize();
+			Dashboard.GetRequests(InitializeTable, (obj) => { Console.WriteLine(obj.Message); });
 		}
 
-		void Initialize()
-		{
-			Dashboard.GetRequests(AddRows, (obj) => { Console.WriteLine(obj.Message); });
-		}
-
-		void AddRows()
+		void InitializeTable()
 		{
 			var table = FindViewById<TableLayout>(Resource.Id.tableLayout);
 			var inflater = (LayoutInflater)GetSystemService(LayoutInflaterService);
 			for (int section = 0; section < Dashboard.RequestLists.Length; section++) {
-				int arrayIndex = Dashboard.SectionToArray[section];
-				var requestList = Dashboard.RequestLists[arrayIndex];
-				var headerView = (TableRow)inflater.Inflate(Resource.Layout.RequestHeader, null);
-				headerView.SetBackgroundColor(Dashboard.SectionBackgroundColors[arrayIndex].GetAndroidColor());
-				var headerLabel = headerView.FindViewById<TextView>(Resource.Id.headerText);
-				headerLabel.Text = Dashboard.SectionTitles[arrayIndex];
-				if (Dashboard.RequestLists != null) {
-					headerLabel.Text += " (" + Dashboard.RequestLists[arrayIndex].Length + ")";
-				}
+				var header = GetHeaderForSection(section);
+				table.AddView(header);
+				var requestList = Dashboard.RequestLists[section];
 				for (int row = 0; row < requestList.Length; row++) {
-					var request = requestList[arrayIndex];
-					var rowView = (TableRow)inflater.Inflate(Resource.Layout.RequestRow, null);
-
-					var description = rowView.FindViewById<TextView>(Resource.Id.descriptionText);
-					var locationLabel = rowView.FindViewById<TextView>(Resource.Id.locationText);
-					var timeLabel = rowView.FindViewById<TextView>(Resource.Id.timeText);
-					var statusLabel = rowView.FindViewById<TextView>(Resource.Id.statusText);
-					//var urgentIndicator = rowView.FindViewById<TextView>(Resource.Id.urgentIndicator);
-					description.Text = request.description;
-					locationLabel.Text = request.room;
-					timeLabel.Text = request.GetTime(Request.TimeType.Received) ?? "N/A";
-					statusLabel.Text = request.GetStatus().ToString();
-					//urgentIndicator.visi = !request.urgent;
-
-					table.AddView(rowView, section);
+					var tableRow = GetTableRow(row, section);
+					table.AddView(tableRow, section);
 				}
 			}
+		}
+
+		View GetHeaderForSection(int section)
+		{
+			var inflater = (LayoutInflater)GetSystemService(LayoutInflaterService);
+			var headerView = (TableRow)inflater.Inflate(Resource.Layout.RequestHeader, null);
+			int arrayIndex = Dashboard.SectionToArray[section];
+			headerView.SetBackgroundColor(Dashboard.SectionBackgroundColors[arrayIndex].GetAndroidColor());
+			var headerLabel = headerView.FindViewById<TextView>(Resource.Id.headerText);
+			headerLabel.Text = Dashboard.SectionTitles[arrayIndex];
+			if (Dashboard.RequestLists != null) {
+				headerLabel.Text += " (" + Dashboard.RequestLists[arrayIndex].Length + ")";
+			}
+			return headerView;
+		}
+
+		TableRow GetTableRow(int row, int section)
+		{
+			var inflater = (LayoutInflater)GetSystemService(LayoutInflaterService);
+			int arrayIndex = Dashboard.SectionToArray[section];
+			var requestList = Dashboard.RequestLists[arrayIndex];
+			var request = requestList[row];
+			var rowView = (TableRow)inflater.Inflate(Resource.Layout.RequestRow, null);
+			var description = rowView.FindViewById<TextView>(Resource.Id.descriptionText);
+			var locationLabel = rowView.FindViewById<TextView>(Resource.Id.locationText);
+			var timeLabel = rowView.FindViewById<TextView>(Resource.Id.timeText);
+			var statusLabel = rowView.FindViewById<TextView>(Resource.Id.statusText);
+			//var urgentIndicator = rowView.FindViewById<TextView>(Resource.Id.urgentIndicator);
+			description.Text = request.description;
+			locationLabel.Text = request.room;
+			timeLabel.Text = request.GetTime(Request.TimeType.Received) ?? "N/A";
+			statusLabel.Text = request.GetStatus().ToString();
+			//urgentIndicator.visi = !request.urgent;
+			return rowView;
 		}
 	}
 }
